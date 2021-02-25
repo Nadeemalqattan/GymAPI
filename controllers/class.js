8ddb9fe2ef0b7a35364551849a15202f60daab19
JannatJameel <j_annat@outlook.com>
2021-02-25 11:26:46 +0300

    we can book but you cant see

--- controllers/class.js
+++ controllers/class.js
@@ -1,4 +1,22 @@
-const { User, Class } = require("../db/models");
+const { User, Class, UserClasses } = require("../db/models");
+
+exports.fetchClass = async (classId, next) => {
+  try {
+    const foundClass = await Class.findByPk(classId, {
+      include: {
+        model: User,
+        as: "users",
+        attributes: ["id"],
+        through: {
+          attributes: ["userId", "classId"],
+        },
+      },
+    });
+    return foundClass;
+  } catch(error) {
+    next(error);
+  }
+};
 
 exports.classList = async (req, res, next) => {
     try {
@@ -6,7 +24,7 @@ exports.classList = async (req, res, next) => {
         include: {
           model: User,
           as: "users",
-        //   attributes: ["id"],
+          attributes: ["id"],
           through: {
             attributes: ["userId", "classId"],
           },
@@ -16,4 +34,32 @@ exports.classList = async (req, res, next) => {
     } catch (error) {
       next(error);
     }
-  };
+};
+
+exports.bookClass = async (req, res, next) => {
+  // const bookedClasses = await Class.findAll({
+  //   include: {
+  //     model: User,
+  //     as: "users",
+  //     attributes: ["id"],
+  //     through: {
+  //       attributes: ["userId", "classId"],
+  //     },
+  //   },
+  //   where: {
+  //     users: 2
+  //   },
+  // });
+  // console.log("User booked", bookedClasses);
+  try {
+    const foundClass = await Class.findByPk(req.body.id);
+    foundClass.addUser(req.body.users);
+    res.status(201).json(foundClass);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.classDetail = async (req, res, next) => {
+  res.json(req.class);
+};
--- routes/classes.js
+++ routes/classes.js
@@ -2,6 +2,19 @@ const express = require("express");
 const router = express.Router();
 const controllers = require("../controllers/class");
 
+router.param("classId", async (req, res, next, classId) => {
+    const foundClass = await controllers.fetchClass(classId, next);
+    if (foundClass) {
+      req.class = foundClass;
+      next();
+    } else next({ status: 404, message: "Class Not Found." });
+});
+
+router.get("/:classId", controllers.classDetail);
+
 router.get("/", controllers.classList);
+
+router.post("/bookclass", controllers.bookClass);
+
 
 module.exports = router;
